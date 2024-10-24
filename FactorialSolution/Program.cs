ba9191f4291ae6989f72af3c13156c7ce9108719
cmramsey3 <coltonramsey3@gmail.com>
2024-10-24 10:49:35 -0400

    Added two Methods

--- FactorialSolution/Program.cs
+++ FactorialSolution/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,15 +11,36 @@ namespace FactorialSolution
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(Factorial(9));
+            Console.WriteLine(HasA("Lucas"));
         }
         /// <summary>
         /// Compute a factorial of an int
         /// </summary>
-        /// <param name="num">The integer to use</param>
+        /// <param name="num">The integer to use (non-negative)</param>
         /// <returns>The factorial of num</returns>
-        static int factorial(int num)
+        static int Factorial(int num)
         {
-
+            int factorial = 1;
+            int counter = 1;
+            while (counter <= num)
+            {
+                factorial *= counter;
+                counter = counter + 1;
+            }
+            return factorial;
+        }
+        /// <summary>
+        /// Check to see if a string has an A in it
+        /// </summary>
+        /// <param name="pw">String to check</param>
+        /// <returns>True if String has A in it False if not</returns>
+        static bool HasA(string pw)
+        {
+            if (pw.ToUpper().Contains("A"))
+                return true;
+            else
+                return false;
         }
     }
 }
