7a46c9f533e2bf495fbb05696e47dc2234946ab3
nicomp42 <nicholdw@ucmail.uc.edu>
2024-10-24 10:16:26 -0400

    Add project files.

---
+++ FactorialSolution/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialSolution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        /// <summary>
+        /// Compute a factorial of an int
+        /// </summary>
+        /// <param name="num">The integer to use</param>
+        /// <returns>The factorial of num</returns>
+        static int factorial(int num)
+        {
+
+        }
+    }
+}
